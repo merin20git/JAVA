5fe2a3c0ec7508d2a2376fbb9724099b74f1c32a
merin20git <merin20122003@gmail.com>
2025-03-07 16:22:43 +0530

    Add files via upload

---
+++ Str.java
@@ -0,0 +1,11 @@
+import java.util.scanner;
+class Str{
+public static void main(String[] args){
+String str1,str2;
+Scanner sc = new Scanner(System.in);
+System.out.print("Enter string1:");
+String str1=sc.nextLine();
+System.out.print("Enter string2:");
+String str2=sc.nextLine();
+}
+}
